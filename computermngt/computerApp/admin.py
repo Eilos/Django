518596151e276dd2380952ab4b5fe5e81f423595
Eilos <93578198+Eilos@users.noreply.github.com>
2022-06-13 09:06:53 +0200

    Add files via upload

---
+++ computermngt/computerApp/admin.py
@@ -0,0 +1,8 @@
+import imp
+from django.contrib import admin
+from .models import Machine, Personnel, infrastructure
+# Register your models here.
+
+admin.site.register(Machine)
+admin.site.register(Personnel)
+admin.site.register(infrastructure)
